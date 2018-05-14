6ac6588332787642b5853b54a6327604a4def7d9
Ben Zeise <ben.zeise@datamigrationresources.com>
2018-05-14 17:22:25 -0500

    initial test commit, edited Cake example scripts

---
+++ .build.ps1
@@ -0,0 +1,4 @@
+      PID    PPID    PGID     WINPID   TTY         UID    STIME COMMAND
+    20844    9740   20844      24332  pty0      197610 11:07:58 /usr/bin/PS
+     9740    4476    9740      17784  pty0      197610   May 11 /usr/bin/bash
+     4476       1    4476       4476  ?         197610   May 11 /usr/bin/mintty
